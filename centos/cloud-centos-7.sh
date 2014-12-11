341c11499fc968a6b50411b9ce4b0037dbe6b83b
Ioannis Panousis <ipanousis@users.noreply.github.com>
2014-12-11 17:05:52 +0000

    Update cloud-centos-7.sh

--- centos/cloud-centos-7.sh
+++ centos/cloud-centos-7.sh
@@ -12,7 +12,7 @@ systemctl restart sshd
 
 # install zfs for flocker
 KERNEL_RELEASE=`uname -r`
-yum install -y https://download.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-2.noarch.rpm
+yum install -y https://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
 yum install -y kernel-devel-$KERNEL_RELEASE kernel-headers-$KERNEL_RELEASE 
 yum localinstall -y --nogpgcheck http://archive.zfsonlinux.org/epel/zfs-release.el7.noarch.rpm
 yum install -y zfs
